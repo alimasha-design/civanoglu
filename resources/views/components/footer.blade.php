48cb7468da81827ea577349aa1e0f31a2928cf4a
Ali <alimashadesign@gmail.com>
2021-12-07 14:10:46 +0600

    Latest commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,90 @@
+<div class="container">
+    <div class="pt-20">
+        <h2 class="section-heading">Articles & Advices</h2>
+        <div class="flex -mx-4">
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Renting in Dubai Compared to the UK</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Renting in Dubai Compared to the UK</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Renting in Dubai Compared to the UK</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pt-20 text-lg border-b-2 border-gray-500 pb-16 mb-20">
+        <h2 class="section-heading">Additional Services</h2>
+        <div class="flex -mx-4">
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Trust investments</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Beautiful suitable land</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Big sale for apartment</a>
+            </div>
+            <div class="flex-1 px-4">
+                <a href="" class="underline">Most attractive suits</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex pb-12">
+        <div class="w-1/4 pr-6">
+            <img width="50" src="/img/logo-new.png" alt="">
+            <p class="text-sm mt-12">Copyright @ Alimasha Design 2021</p>
+        </div>
+
+        <div class="w-3/4 font-bold leading-8">
+            <div class="flex -mx-6">
+                <div class="flex-1 px-6">
+                    <ul>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                    </ul>
+                </div>
+                <div class="flex-1 px-6">
+                    <ul>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                    </ul>
+                </div>
+                <div class="flex-1 px-6">
+                    <ul>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                        <li><a href="">Lorem ipsum dolor</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<img src="/img/footer.jpg" alt="">
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,178 @@
+<x-guest-layout>
+    <div class="relative z-10 pt-52 pb-64 bg-cover bg-center" style="background-image: url(/img/hero-bg.jpg)">
+        <div class="absolute h-full w-full bg-black opacity-50 top-0 left-0 z-10"></div>
+        <div class="container relative z-20 text-white text-center text-2xl">
+            <h2 class="font-bold text-5xl mb-5">Exclusive Apartment Design <br/> Best Land Property With Suitable Price</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+        </div>
+    </div>
+
+    <!-- Search From Area -->
+    <div class="rounded-lg container bg-white p-4 relative -m-12 shadow-lg">
+        <form action="#" method="GET" class="flex justify-between">
+            @csrf
+            <div class="flex w-7/12 justify-between">
+                <div class="flex flex-col mx-3">
+                    <select name="" id="" class="border-0">
+                        <option value="#">Buy or Rent</option>
+                        <option value="#">Buy</option>
+                        <option value="#">Rent</option>
+                    </select>
+                    <small class="pl-3">Buy</small>
+                </div>
+                <div class="py-3 self-center border-gray-500 border-2"></div>
+                <div class="flex flex-col mx-3">
+                    <select name="" id="" class="border-0">
+                        <option value="#">Type</option>
+                        <option value="#">Buy</option>
+                        <option value="#">Rent</option>
+                    </select>
+                    <small class="pl-3">Appointment</small>
+                </div>
+                <div class="py-3 self-center border-gray-500 border-2"></div>
+                <div class="flex flex-col mx-3">
+                    <select name="" id="" class="border-0">
+                        <option value="#">Price</option>
+                        <option value="#">Buy</option>
+                        <option value="#">Rent</option>
+                    </select>
+                    <small class="pl-3">1,000 - 1,200</small>
+                </div>
+                <div class="py-3 self-center border-gray-500 border-2"></div>
+                <div class="flex flex-col mx-3">
+                    <select name="" id="" class="border-0">
+                        <option value="#">Bedrooms</option>
+                        <option value="#">Buy</option>
+                        <option value="#">Rent</option>
+                    </select>
+                    <small class="pl-3">3 and more</small>
+                </div>
+            </div>
+            <div class="flex justify-between items-center w-5/12 ml-5">
+                <input type="search" placeholder="Try to search for something" class="rounded-lg px-4 py-2 w-full mr-4">
+                <button type="submit" class="btn">Search</button>
+            </div>
+        </form>
+    </div>
+
+    <div class="py-32 text-center">
+        <div class="container">
+            <h2 class="font-bold text-4xl mb-6">Looking for Gorgeous Apartment for Living <br/> Smart Thinking Right Decision Make <span class="underline">Feeling Better</span> </h2>
+            <p class="text-gray-500 text-2xl mb-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+            <a class="border-2 border-gray-500 rounded-2xl text-xl px-8 py-3 inline-block" href="">Start the Searching</a>
+        </div>
+    </div>
+
+    <div class="mb-6 mt-20 container text-center">
+        <h2 class="font-bold text-4xl">Smart Thinking Right Decision Make <br/> Gorgeous Apartment for Living <span class="underline">Feeling Better</span> </h2>
+        <div class="flex -mx-3 flex-wrap justify-between mt-10">
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+        </div>
+    </div>
+    <div class="container text-center pt-14">
+        <h2 class="section-title">More information about us</h2>
+        <div class="relative mt-10 mb-14 bg-cover rounded-xl py-24 bg-center"
+             style="background-image: url('/img/hero-bg.jpg')">
+            <div class="absolute w-full h-full rounded-xl opacity-50 bg-black left-0 top-0"></div>
+            <div class="relative z-20">
+                <a href="" class="text-white text-xl flex flex-col justify-center items-center"><span class="border-2 border-white w-12 h-12 text-center pt-1 pl-1 leading-10 text-2xl hover:border-yellow-500 duration-200 rounded-full mb-2"><i class="fa fa-play"></i></span>Watch the video</a>
+            </div>
+        </div>
+
+        <div class="text-xl">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto autem corporis debitis
+                dignissimos doloremque eius error explicabo impedit iste libero magnam, natus officiis omnis quasi
+                ratione sequi totam, vero.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto
+                autem corporis debitis
+                dignissimos doloremque eius error explicabo impedit iste libero magnam, natus officiis omnis quasi
+                ratione sequi totam, vero.</p>
+
+        </div>
+    </div>
+
+
+    <div class="container pt-14">
+        <div class="flex justify-between ">
+            <div class="flex-1 mr-10 text-lg leading-normal">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti ipsam iure laborum nemo
+                    non odit quis quod, sint tempore. Ab culpa cupiditate error, eum incidunt officiis rem sapiente
+                    voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti ipsam iure
+                    laborum nemo non odit quis quod, sint tempore. Ab culpa cupiditate error, eum incidunt officiis rem
+                    sapiente voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti
+                    ipsam iure laborum nemo non odit quis quod, sint tempore. Ab culpa cupiditate error, eum incidunt
+                    officiis rem sapiente voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+                    deleniti ipsam iure laborum nemo non odit quis quod, sint tempore. Ab culpa cupiditate error, eum
+                    incidunt officiis rem sapiente voluptate</p>
+            </div>
+            <div class="flex-1 ml-10">
+                <img src="/img/hero-bg.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="container pt-14">
+        <div class="flex justify-center items-center">
+            <a href="" class="btn">Start searching with filters</a>
+            <p class="mx-10">or</p>
+            <a href="" class="btn-outline">Start searching on the map</a>
+        </div>
+    </div>
+
+    <!-- Last Added Objects -->
+    <div class="container py-14">
+        <h2 class="section-title">Last added objects</h2>
+        <div class="flex -mx-2 justify-between mt-10">
+
+            @foreach($latest_properties as $property)
+            <div class="flex-1 mx-2 shadow-lg relative rounded-md">
+                <a href=""
+                   class="absolute left-3 w-9 h-9 leading-10 self-center text-base top-3 bg-black text-white bg-opacity-25 text-center hover:bg-yellow-500 hover:text-white duration-200 rounded-full text-base"><i
+                        class="fa fa-heart-o"></i></a>
+                <div class="py-20 bg-center" style="background-image: url('/img/hero-bg.jpg')"></div>
+                <div class="p-3">
+                    <h2 class="leading-0 text-base">{{$property->name}}</h2>
+                    <h3 class="text-2xl py-3">{{$property->price}} TL</h3>
+                    <div class="border-t-2">
+                        <ul class="flex items-center -mx-1 my-4">
+                            <li class="px-2 py-1 bg-blue-50 rounded-md text-xs mx-1 shadow-sm">7 bedrooms</li>
+                            <li class="px-2 py-1 bg-blue-50 rounded-md text-xs mx-1 shadow-sm">5 bedrooms</li>
+                            <li class="px-2 py-1 bg-blue-50 rounded-md text-xs mx-1 shadow-sm">15000 ft<sup>2</sup></li>
+                        </ul>
+                        <a href="{{route('single-property', $property->id)}}" class="btn w-full text-center">More details</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+
+
+        </div>
+    </div>
+</x-guest-layout>
